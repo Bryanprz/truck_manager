9c0150bea50c079fb2614ae0a82f6cbf7236d72c
Bryan Perez <bryan@breakthrough.com>
2016-01-02 00:03:42 -0500

    Removing unecessary test directory since we're using specs

--- test/models/pickup_site_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PickupSiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/truck_capacity_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TruckCapacityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
