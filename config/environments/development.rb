1d3df5c76e11160b9803bab35fa50d5949f68570
Bryan Perez <bryan@breakthrough.com>
2016-01-05 00:52:03 -0800

    Add pry config to dev env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 end
 
-#silence_warnings do
-  #require 'pry'
-  #IRB = Pry
-#end
+silence_warnings do
+  require 'pry'
+  IRB = Pry
+end
