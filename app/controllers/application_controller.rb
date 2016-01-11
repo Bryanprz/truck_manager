99755971e901b9bb50e8ddc033f2186cd93a4240
Bryan Perez <bryan@breakthrough.com>
2016-01-10 18:49:38 -0800

    Add navbar to site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_clients
+
+  def set_clients
+    @clients = Client.all
+  end
 end
